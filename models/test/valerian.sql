0bea9b8102d340837326c3ee1a8d34ab9197f6ed
everyonehatesLin3lis <85938879+everyonehatesLin3lis@users.noreply.github.com>
2022-10-13 13:20:42 +0300

    Update valerian.sql

--- models/test/valerian.sql
+++ models/test/valerian.sql
@@ -1,2 +1,4 @@
-SELECT JSON_QUERY(_airbyte_data, '$.total_price_usd') AS total_price_in_usd
+SELECT JSON_QUERY(_airbyte_data, '$.id') AS id,
+       JSON_QUERY(_airbyte_data, '$.currency') AS currency,
+       JSON_QUERY(_airbyte_data, '$.processed_at') AS processed_at,
 FROM `Linas_DBT_test._airbyte_raw_orders`
