07fae8c190687d618768f41395723bef0d2e5b7c
everyonehatesLin3lis <85938879+everyonehatesLin3lis@users.noreply.github.com>
2022-10-13 12:56:59 +0300

    Update valerian.sql

--- models/test/valerian.sql
+++ models/test/valerian.sql
@@ -1,5 +1,2 @@
-SELECT _airbyte_data ->> 'id' AS id, 
-    _airbyte_data ->> 'currency' AS currency,
-    _airbyte_data ->> 'refunds' AS refunds,
-    _airbyte_data -> 'customer' ->> 'id' as customer_id
+SELECT JSON_QUERY(_airbyte_data, '$.total_price_usd') AS total_price_in_usd
 FROM test._airbyte_raw_orders
