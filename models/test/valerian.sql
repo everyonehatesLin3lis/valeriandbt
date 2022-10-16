89f5d7ec0eca37bf9e82d1ed322f1688933f8ade
everyonehatesLin3lis <peleckas.l@gmail.com>
2022-10-16 20:38:58 +0300

    new

--- models/test/valerian.sql
+++ models/test/valerian.sql
@@ -1,4 +1,4 @@
 SELECT JSON_QUERY(_airbyte_data, '$.id') AS id,
        JSON_QUERY(_airbyte_data, '$.currency') AS currency,
        JSON_QUERY(_airbyte_data, '$.processed_at') AS processed_at,
-FROM `Linas_DBT_test._airbyte_raw_orders`
+FROM `Linas_DBT_test._airbyte_raw_orders`.
