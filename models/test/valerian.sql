29e5d193b2b2ca6d5feca9ccad91cac99a0d18d3
everyonehatesLin3lis <peleckas.l@gmail.com>
2022-10-16 22:01:12 +0300

    idk

--- models/test/valerian.sql
+++ models/test/valerian.sql
@@ -1,14 +1,5 @@
-WITH Refunds AS (
-    SELECT JSON_QUERY(_airbyte_data, '$.refunds') AS refunds,
-        
-    FROM
-        `Linas_DBT_test._airbyte_raw_orders`
-)
-
 SELECT JSON_QUERY(_airbyte_data, '$.id') AS id,
        JSON_EXTRACT_SCALAR(_airbyte_data, '$.currency') AS currency,
        JSON_EXTRACT_SCALAR(_airbyte_data, '$.processed_at') AS processed_at,
-       JSON_EXTRACT_SCALAR(r, '$.transactions[0].id') refund_id
+       JSON_EXTRACT_SCALAR(_airbyte_data, '$.refunds.transactions[0].id') AS refund_id
 FROM `Linas_DBT_test._airbyte_raw_orders`
-LEFT JOIN Refunds r
-
