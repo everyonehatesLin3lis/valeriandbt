1cddf7e04b128eeac98269f9a22e22e185dd93fa
everyonehatesLin3lis <peleckas.l@gmail.com>
2022-10-17 09:19:14 +0300

    new

--- models/test/valerian.sql
+++ models/test/valerian.sql
@@ -1,5 +1,5 @@
 SELECT JSON_QUERY(_airbyte_data, '$.id') AS id,
        JSON_EXTRACT_SCALAR(_airbyte_data, '$.currency') AS currency,
        JSON_EXTRACT_SCALAR(_airbyte_data, '$.processed_at') AS processed_at,
-       JSON_QUERY(_airbyte_data, '$.refunds[0].transactions[0].id') AS refund_id
+       JSON_QUERY(_airbyte_data, '$.refunds[0].id') AS refund_id
 FROM `Linas_DBT_test._airbyte_raw_orders`
