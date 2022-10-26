3cd25cb26580b2847d92279e106f7726a4cd6587
everyonehatesLin3lis <peleckas.l@gmail.com>
2022-10-26 14:10:59 +0300

    NEW

--- models/test/valerian.sql
+++ models/test/valerian.sql
@@ -1,3 +1,4 @@
 SELECT JSON_QUERY(_airbyte_data, '$.id') AS id,
-       JSON_EXTRACT_SCALAR(_airbyte_data, '$.created_at') AS created_at
+       JSON_EXTRACT_SCALAR(_airbyte_data, '$.created_at') AS created_at,
+       JSON_EXTRACT_SCALAR(_airbyte_data, '$.total_price') AS total_price
 FROM `Linas_DBT_test._airbyte_raw_orders`
